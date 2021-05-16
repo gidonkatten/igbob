dbfa592f49f4d9fc2d5cc5841aa4384278c83e61
Gidon <gidonkatten@gmail.com>
2021-05-16 16:36:19 +0100

    Home page text

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -6,8 +6,11 @@ interface HomePageProps {
 function HomePage(props: HomePageProps) {
 
   return (
-    <div>
-      Welcome to Green Bonds on the Blockchain. This is a demo application. Get started by creating an account.
+    <div className={"page-content"}>
+      <div className={'home-text'}>
+        <h1>Welcome to Issuing Green Bonds on the Blockchain</h1>
+        <h3>Demo application</h3>
+      </div>
     </div>
   );
 }
